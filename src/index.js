dac2ec43fcb2d5f423dbcbbc2e81b6c6c1f23c55
Pavlenko-Oksana <65184570+Pavlenko-Oksana@users.noreply.github.com>
2020-11-14 13:41:58 +0200

    добавляет шаблон разметки.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import './styles.css';
+import countryTemplate from './templates/country-card.hbs';
+
+console.log(countryTemplate);
+
+const refs = {
+    marcupContainer: document.querySelector('.js-card-container'),
+}
+
+
 
 fetch(`https://restcountries.eu/rest/v2/name/ukraine`).then(response => {
     return response.json();
 }).then(country => {
-    console.log(country)
+    console.log(country[0]);
+    const marcup = countryTemplate(country[0]);
+    console.log(marcup);
+    refs.marcupContainer.innerHTML = marcup;
+    
+}).catch(error => {
+    console.log(error)
 });
